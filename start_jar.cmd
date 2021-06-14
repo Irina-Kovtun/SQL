55028d91ef81485a2686b4ad9054b3c7d2d1f76f
Irina-Kovtun <irinakovtun.us@gmail.com>
2021-06-14 15:09:28 +0300

    service files are added

--- start_jar.cmd
+++ start_jar.cmd
@@ -3,6 +3,5 @@
 @REM set DB_PASS=pass
 @REM -P:jdbc.url=..., -P:jdbc.user=..., -P:jdbc.password=...
 @REM docker-compose exec mysql mysql -u user --password=pass --database=app_db --execute="source /docker-entrypoint-initdb.d/schema_2.sql;exit;"
-java -jar app-deadline.jar -P:jdbc.url=jdbc:mysql://localhost:3306/app -P:
-jdbc.user=app -P:jdbc.password=pass
+java -jar app-deadline.jar -P:jdbc.url=jdbc:mysql://localhost:3306/app -P:jdbc.user=app -P:jdbc.password=pass
 
